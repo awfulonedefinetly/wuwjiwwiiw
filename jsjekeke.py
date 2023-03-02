495051b12da13a94c98f7733a2309a768c2b9074
awfulonedefinetly <124203718+awfulonedefinetly@users.noreply.github.com>
2023-03-02 18:48:26 +0100

    Create jsjekeke.py

---
+++ jsjekeke.py
@@ -0,0 +1,37 @@
+import sys 
+import json 
+import datetime 
+import random 
+import math 
+import time 
+import requests
+import os.path as path 
+import cv2 
+import websocket 
+from colorama import Fore, Back, Style, init 
+
+init(convert=True)
+
+class PlaceBotConfigAuth(object): 
+     def __init__(self): 
+         self.login = '' 
+         self.password = '' 
+ class PlaceBotConfigProxy(object): 
+     def __init__(self): 
+         self.host = '' 
+         self.port = 0 
+         self.user = '' 
+         self.passwd = '' 
+ class PlaceBotConfigImage(object): 
+     def __init__(self): 
+         self.path = '' 
+         self.x = 0 
+         self.y = 0 
+         self.defend = False 
+         self.strategy = '' 
+         self.canv_id = 0 
+ class PlaceBotConfig(object): 
+     def __init__(self): 
+         self.auth  = PlaceBotConfigAuth() 
+         self.proxy = PlaceBotConfigProxy() 
+         self.image = PlaceBotConfigImage()
